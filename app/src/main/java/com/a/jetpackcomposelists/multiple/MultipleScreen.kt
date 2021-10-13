ec725b4d1f080be8846308edff89f3ab518b424d
Cabezas <javiergonzalezcabezas@gmail.com>
2021-10-13 21:24:05 +0200

    added scaffold in multiple

--- app/src/main/java/com/a/jetpackcomposelists/multiple/MultipleScreen.kt
+++ app/src/main/java/com/a/jetpackcomposelists/multiple/MultipleScreen.kt
@@ -7,8 +7,10 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Checkbox
+import androidx.compose.material.Scaffold
 import androidx.compose.material.TabRowDefaults.Divider
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,7 +19,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.a.jetpackcomposelists.R
 
 
 @Composable
@@ -26,8 +30,20 @@ fun MultipleScreen(
     context: Context,
     completed: MutableList<Boolean> = MutableList(viewModel.getInitial(context).size) { false }
 ) {
-    Column(modifier = Modifier.fillMaxHeight()) {
-        NameList(viewModel, context, Modifier.weight(1f), completed)
+    Scaffold(
+        modifier = Modifier
+            .fillMaxWidth(),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.app_name))
+                }
+            )
+        }
+    ) {
+        Column(modifier = Modifier.fillMaxHeight()) {
+            NameList(viewModel, context, Modifier.weight(1f), completed)
+        }
     }
 }
 
