926528e842d690f2cc2da960fcc16978a5a54d88
Cabezas <javiergonzalezcabezas@gmail.com>
2021-10-30 09:38:36 +0200

    finished single choice

--- app/src/main/java/com/a/jetpackcomposelists/single/SingleScreen.kt
+++ app/src/main/java/com/a/jetpackcomposelists/single/SingleScreen.kt
@@ -5,18 +5,19 @@ import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.selection.selectable
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.RadioButton
-import androidx.compose.material.TabRowDefaults
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
-
+import androidx.compose.ui.unit.sp
+import com.a.jetpackcomposelists.R
 
 @Composable
 fun SingleScreen(
@@ -37,27 +38,35 @@ fun NameList(
 ) {
     val selectedOption = mutableStateOf(0)
     val optionsList = viewModel.getInitial(context)
-
-    LazyColumn(modifier = modifier.height(90.dp)) {
-        items(items = optionsList) { item: String ->
-            ElementSelected(text = item,
-                selectedValue =  optionsList[selectedOption.value],
-                onClickListener = {
-                    Toast.makeText(context, item, Toast.LENGTH_SHORT).show()
-                })
-            TabRowDefaults.Divider(color = Color.LightGray)
+    Scaffold(modifier = Modifier.fillMaxWidth(), topBar = {
+        TopAppBar(title = {
+            Text(text = stringResource(id = R.string.app_name))
+        }, actions = {})
+    }) {
+        LazyColumn(modifier = modifier.fillMaxSize()) {
+            itemsIndexed(items = optionsList) { selected: Int, item: String ->
+                ElementSelected(text = item,
+                    selectedValue = optionsList[selectedOption.value],
+                    onClickListener = {
+                        selectedOption.value = selected
+                        Toast.makeText(context, item, Toast.LENGTH_SHORT).show()
+                    })
+                TabRowDefaults.Divider(color = Color.LightGray)
+            }
         }
     }
 }
 
 
-
 @Composable
-fun ElementSelected(text: String,
-                    selectedValue: String,
-                    onClickListener: (String) -> Unit) {
-    Row(
+fun ElementSelected(
+    text: String,
+    selectedValue: String,
+    onClickListener: (String) -> Unit
+) {
+    Box(
         Modifier
+            .height(90.dp)
             .fillMaxWidth()
             .selectable(
                 selected = (text == selectedValue),
@@ -66,14 +75,20 @@ fun ElementSelected(text: String,
                 })
     ) {
         RadioButton(
+            modifier = Modifier
+                .align(Alignment.CenterStart)
+                .padding(10.dp),
             selected = (text == selectedValue),
             onClick = {
                 onClickListener(text)
             })
         Text(
             text = text,
-            style = MaterialTheme.typography.body1.merge(),
-            modifier = Modifier.padding(start = 16.dp)
+            fontSize = 30.sp,
+            fontStyle = FontStyle.Italic,
+            modifier = Modifier
+                .padding(start = 16.dp)
+                .align(Alignment.Center)
         )
     }
 }
